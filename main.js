6c769488d8c6b3f706383747f037e327a0470e17
Zac Atsatt <contact@ujlee.com>
2016-03-02 00:16:23 -0800

    second commit

--- main.js
+++ main.js
@@ -16,12 +16,16 @@ function sendRequest(event) {
   country = $('#searchCountry').val();
   zipCode = $('#searchZip').val();
   units = $('#unitInput').val();
-  console.log(city, country, zipCode, units);
-  searchCities(city, country, zipCode, units);
 
+  if(country.length > 2){
+    alert('Please enter country code in two (2) character format!');
+  } else {
+  getCurrentWeather(city, country, zipCode, units);
+  //getFiveDayForecast(city, country, zipCode, units);
+  }
 }
 
-function searchCities(a, b, c, d) {
+function getCurrentWeather(a, b, c, d) {
   var city = 'q=' + a;
   var country = ',' + b;
   var zip = '&zip=' + c;
@@ -31,11 +35,24 @@ function searchCities(a, b, c, d) {
     method: 'GET',
     url: 'http://api.openweathermap.org/data/2.5/find?' + city + country + zip + units + '&APPID=48bfdad1557309316103e40e659956ac',
     success: function(data) {
-      console.log('succes: ', data);
+      console.log('success: ', data.list);
 
     },
     error: function(error) {
       console.error('error: ', error);
     }
   });
+}
+
+function addWeatherInfo(data) {
+      var $weather = $('#template').clone();
+
+      $weather.find('#tempurature').text(data.main.main.temp);
+      //$weather.find('#precip').text(data.rain.rain.3h);
+      $weather.find('#humidity').text(data.main.main.humidity);
+      $weather.find('#wind').text(data.wind.wind.speed)
+      $weather.find('#wIcon').attr('href', function() {
+        return 'http://openweathermap.org/img/w/' + data.weather.icon + '.png';
+      })
+      return $weather;
 }
